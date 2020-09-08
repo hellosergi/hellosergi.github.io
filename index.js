7bf88aa3b8f0d1581ddf7c1a9a29be23a0612cc7
hellosergi <rockncream@gmail.com>
2020-09-08 13:25:24 +0300

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-import "splitting/dist/splitting.css";
-import "splitting/dist/splitting-cells.css";
-import Splitting from "splitting";
+var parentPosX;
+var parentPosY;
 
-Splitting();
+var parent = document.getElementById('asterisk');
+var rectParent = parent.getBoundingClientRect();
+// console.log(rectParent.top, rectParent.right, rectParent.bottom, rectParent.left);
+
+var child = document.getElementById('curved');
+var rectChild = curved.getBoundingClientRect();
+// console.log(rectChild.top, rectChild.right, rectChild.bottom, rectChild.left);
+
+
+// child.style.top = rectParent.top + 20 + "px"
+// child.style.left = rectParent.left + 20 + "px";
